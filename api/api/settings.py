b7dbd40ba04e5a61da86eec55d82fc4928196e83
Saahil Ali <programmer290399@gmail.com>
2019-03-25 09:35:47 +0530

    Added API versioning

--- api/api/settings.py
+++ api/api/settings.py
@@ -120,3 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+REST_FRAMEWORK = {
+    # When you enable API versioning, the request.version attribute will contain a string
+    # that corresponds to the version requested in the incoming client request.
+    'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.URLPathVersioning',
+}
